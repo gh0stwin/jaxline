042728c13081da41a00dbe29cb013b4246093edb
Fábio Vital <fabiovital@tecnico.ulisboa.pt>
2022-07-08 09:22:00 +0000

    neptune ai checkpointer

--- jaxline/utils.py
+++ jaxline/utils.py
@@ -23,11 +23,12 @@ import enum
 import functools
 import os
 import pdb
+import pickle
 import queue
 import sys
 import threading
 
-from typing import Any, Callable, Dict, Generator, Iterable, Iterator, Mapping, Optional, TypeVar
+from typing import Any, Callable, Dict, Generator, Iterable, Iterator, Mapping, Optional, TypeVar, Union
 
 from absl import flags
 from absl import logging
@@ -94,27 +95,48 @@ class Writer(abc.ABC):
     """Write config to logger. By default ignore it."""
     return
 
-class Checkpointer(Protocol):
+class Checkpointer(abc.ABC):
   """An interface for checkpointer objects."""
 
+  @abc.abstractmethod
   def save(self, ckpt_series: str) -> None:
     """Saves the checkpoint."""
 
+  @abc.abstractmethod
   def restore(self, ckpt_series: str) -> None:
     """Restores the checkpoint."""
 
+  @abc.abstractmethod
   def get_experiment_state(self, ckpt_series: str):
     """Returns the experiment state for a given checkpoint series."""
 
+  @abc.abstractmethod
   def restore_path(self, ckpt_series: str) -> Optional[str]:
     """Returns the restore path for the checkpoint, or None."""
 
+  @abc.abstractmethod
   def can_be_restored(self, ckpt_series: str) -> bool:
     """Returns whether or not a given checkpoint series can be restored."""
 
   def wait_for_checkpointing_to_finish(self) -> None:
     """Waits for any async checkpointing to complete."""
+    return
 
+  def _override_or_insert(self, current_state, snapshot):
+    """Update the current state based on a snapshot."""
+    for sk, sv in snapshot.items():
+      # Duck-typing for "is this a Jaxline Experiment class?".
+      if (sk in current_state
+          and hasattr(current_state[sk], "CHECKPOINT_ATTRS")
+          and hasattr(current_state[sk], "NON_BROADCAST_CHECKPOINT_ATTRS")):
+        for kk in sv.CHECKPOINT_ATTRS:
+          setattr(current_state[sk], kk, getattr(sv, kk))
+        for kk in sv.NON_BROADCAST_CHECKPOINT_ATTRS:
+          setattr(
+              current_state[sk], kk,
+              jax.tree_map(copy.copy, getattr(sv, kk)))
+      else:
+        current_state[sk] = sv
 
 class DataPrefetcher:
   """Performs prefetching of elements from an iterable in a separate thread through
@@ -617,28 +639,12 @@ def evaluate_should_return_dict(f: F) -> F:
 GLOBAL_CHECKPOINT_DICT = {}
 
 
-class InMemoryCheckpointer:
+class InMemoryCheckpointer(Checkpointer):
   """A Checkpointer reliant on an in-memory global dictionary."""
 
   def __init__(self, config, mode: str):
     self._max_checkpoints_to_keep = config.max_checkpoints_to_keep
     del mode
-
-  def _override_or_insert(self, current_state, snapshot):
-    """Update the current state based on a snapshot."""
-    for sk, sv in snapshot.items():
-      # Duck-typing for "is this a Jaxline Experiment class?".
-      if (sk in current_state
-          and hasattr(current_state[sk], "CHECKPOINT_ATTRS")
-          and hasattr(current_state[sk], "NON_BROADCAST_CHECKPOINT_ATTRS")):
-        for kk in sv.CHECKPOINT_ATTRS:
-          setattr(current_state[sk], kk, getattr(sv, kk))
-        for kk in sv.NON_BROADCAST_CHECKPOINT_ATTRS:
-          setattr(
-              current_state[sk], kk,
-              jax.tree_map(copy.copy, getattr(sv, kk)))
-      else:
-        current_state[sk] = sv
 
   def get_experiment_state(self, ckpt_series: str):
     """Returns the experiment state for a given checkpoint series."""
@@ -682,9 +688,82 @@ class InMemoryCheckpointer:
       return None
     return GLOBAL_CHECKPOINT_DICT[ckpt_series].history[-1].id
 
-  def wait_for_checkpointing_to_finish(self) -> None:
-    """Waits for any async checkpointing to complete."""
 
+ACTIVE_CHECKPOINT = {}
+
+
+class NeptuneAiCheckpointer(Checkpointer):
+  def __init__(
+    self,
+    config: Union[Mapping[str, Any], config_dict.ConfigDict],
+    mode: str,
+    project: Optional[str] = None,
+    name: Optional[str] = None,
+    api_token: Optional[str] = None,
+  ) -> None:
+    super().__init__(config, mode)
+    self._tmp_local_dir = config.checkpoint_dir
+    self._base_dir = config.base_dir
+    self._last_model_path = f"{self._base_dir}/last_model_path"
+    self._suffix = ".pkl"
+    self._i = 0
+
+    try:
+      self._run = neptune.get_last_run()
+    except neptune.NeptuneUninitializedException:
+      self._run = None
+
+    if self._run:
+      return
+
+    self._run = neptune.init(
+      project=project,
+      name=name,
+      api_token=api_token,
+      source_files=[],
+      capture_hardware_metrics=False,
+      capture_stdout=False,
+      capture_stderr=False,
+    )
+
+  def can_be_restored(self, ckpt_series: str) -> bool:
+    return (ckpt_series in ACTIVE_CHECKPOINT or
+           self._run[f"{self._base_dir}/{ckpt_series}__last_path"].exists())
+
+  def get_experiment_state(self, ckpt_series: str):
+    if ckpt_series not in ACTIVE_CHECKPOINT:
+      ACTIVE_CHECKPOINT[ckpt_series] = threading.local()
+
+    if not hasattr(self._active_checkpoint, "state"):
+      ACTIVE_CHECKPOINT[ckpt_series].state = config_dict.ConfigDict()
+    
+    return ACTIVE_CHECKPOINT[ckpt_series]
+
+  def save(self, ckpt_series: str) -> None:
+    ckpt_file = os.path.join(self._base_dir, f"checkpoint{self._suffix}")
+
+    with open(ckpt_file, "wb") as checkpoint_file:
+      pickle.dump(ACTIVE_CHECKPOINT[ckpt_series].__dict__, checkpoint_file, protocol=2)
+
+    path = f"{self._base_dir}/{ckpt_series}__{self._i}"
+    self._run[path].upload(ckpt_file)
+    self._run[f"{self._base_dir}/{ckpt_series}__last_path"] = path
+    self._i += 1
+
+  def restore(self, ckpt_series: str) -> None:
+    checkpoint_path = self.restore_path(ckpt_series)
+    self._run[checkpoint_path].download(self._tmp_local_dir)
+    checkpoint_path = os.path.join(
+      self._checkpoint_dir, checkpoint_path.rsplit("/", 1)[-1] + self._suffix
+    )
+
+    with open(checkpoint_path, "rb") as checkpoint_file:
+      checkpoint = pickle.load(checkpoint_file)
+    
+    self._override_or_insert(self.get_experiment_state(ckpt_series), checkpoint)
+
+  def restore_path(self, ckpt_series: str) -> Optional[str]:
+    return self._run[f"{self._base_dir}/{ckpt_series}__last_path"].fetch()
 
 def disable_pmap_jit(fn: F) -> F:
   """Disables pmaps/jits inside a function if `--jaxline_disable_pmap_jit=True`.
